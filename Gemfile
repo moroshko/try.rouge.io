0537f7fd32fc616e243ac8baee1d2ddfd358de07
Arlen Christian Mart Cuss <ar@len.me>
2012-10-24 12:41:57 +1100

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'jquery-rails'
+gem 'haml-rails', '>= 0.3.4'
+gem 'twitter-bootstrap-rails'
+
+group :assets do
+  gem 'sass-rails', '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryRougeIo::Application.config.secret_token = '8e5677e66416bcb6958ff295dc9e91bad94a70d908d0706429edc8b9ae4f315a7d8cb7158874c648699be718ab6f28b0ccc079ec19a020f0ab550aab4d1f630a'
